e3aa45e09c2dc437c304c466c8aa3964634e6f8a
Hrushi <keshwa019@gmail.com>
2020-07-30 10:25:45 +0530

    second commit

--- p6/urls.py
+++ p6/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 import myapp01
+from myapp01 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sam/',views.sam,name="sample"),
+    #path('primmary suffix,include("appname.urls"))
     path('myapp1/',include("myapp01.urls")),
     
 ]
